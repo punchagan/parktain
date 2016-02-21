799ad9ca18d8a39071fff1885cecd157f399435c
baali <baali@muse-amuse.in>
2016-02-21 15:26:48 +0530

    First version of models to save message logs.

---
+++ parktain/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import *
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///slack-archives.db')
+
+engine.echo = False
+
+Base = declarative_base()
+
+class Message(Base):
+    __tablename__ = 'messages'
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    user_id = Column(String, nullable=False, index=True)
+    channel_id = Column(String, nullable=False, index=True)
+    message = Column(String(4096))
+    timestamp = Column(DateTime)
+    
+    def __repr__(self):
+        return "<Message(user='%s' said='%s' in channel='%s')>" % (
+            self.user_id, self.message, self.channel_id)
