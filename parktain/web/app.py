29df2a0cbf1bbb662ecaf21f39158434a1223d63
Puneeth Chaganti <punchagan@muse-amuse.in>
2016-02-24 19:38:50 +0530

    Add a barebones web app with Slack OAuth.

---
+++ parktain/web/app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# Standard library.
+from os.path import abspath, dirname, join
+
+# 3rd party library.
+from flask import Flask, redirect, url_for
+from flask_dance.contrib.slack import make_slack_blueprint, slack
+import yaml
+
+HERE = dirname(abspath(__file__))
+
+def get_app_config():
+    path_to_yaml = join(HERE, '..', 'config.yaml')
+    with open(path_to_yaml, 'r') as ymlfile:
+        web = yaml.load(ymlfile).get('web')
+
+    if not web:
+        web = {'client_id': '', 'client_secret': '', 'secret_key': ''}
+
+    return web
+
+## NOTE: When running locally, export OAUTHLIB_INSECURE_TRANSPORT=1
+## See: http://flask-dance.readthedocs.org/en/latest/quickstarts/slack.html
+
+config = get_app_config()
+app = Flask(__name__)
+app.secret_key = config['secret_key']
+blueprint = make_slack_blueprint(
+    client_id=config['client_id'],
+    client_secret=config['client_secret'],
+    scope=["identify", "chat:write:bot"],
+)
+app.register_blueprint(blueprint, url_prefix="/login")
+
+@app.route("/")
+def index():
+    if not slack.authorized:
+        return redirect(url_for("slack.login"))
+    return 'Hello, there!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
